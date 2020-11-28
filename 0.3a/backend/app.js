94440b3161852bb605832d602fa3711efcf233d3
Jerome-G78 <67152285+Jerome-G78@users.noreply.github.com>
2020-11-28 11:31:09 +0100

    Initial

---
+++ 0.3a/backend/app.js
@@ -0,0 +1,43 @@
+// import des packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+// Ajout des routes pour l'identification & l'authentification
+const userRoutes = require('./routes/user');
+
+// initialisation de la variable app qui contiendra 'express'
+const app = express();
+app.use(cors());
+
+// Connexion a la base de données MongoDB
+/*
+mongoose.connect('mongodb+srv://PoppY:U0gh9JQ5vF1dtykS@cluster0.6rjyj.mongodb.net/Cluster0?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+console.log (app._router);
+*/
+
+// Cette partie configure et autorise les requêtes Multi-Origin; définit les Headers & les Methodes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Définit la fonction json comme middleware global pour l'application
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Enregistrement du routeur pour toutes les demandes effectuées vers /api/auth.
+app.use('/api/auth', userRoutes);
+
+// Permet d'exporter l'application créer (en l'occurence, elle devient accesible pour serveur.js)
+module.exports = app;
