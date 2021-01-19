af8c626f55dbc9ab4746fd69bd3b4f1839d287e1
Jerome-G78 <67152285+Jerome-G78@users.noreply.github.com>
2021-01-19 16:49:24 +0100

    Updating Frontend - SignIn / LogIn / Profil / NewMessage / Likes / Wall & Comments

--- 0.8c/frontend/src/views/Home.vue
+++ 0.8c/frontend/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <HeaderNav/>
-    <Login/>
     <NewMessage/>
     <Profil/>
     <Wall/>
@@ -14,7 +13,6 @@
 <script>
 // @ is an alias to /src
 import HeaderNav from '@/components/HeaderNav.vue'
-import Login from '@/components/Login.vue'
 import NewMessage from '@/components/NewMessage.vue'
 import Profil from '@/components/Profil.vue'
 import Wall from '@/components/Wall.vue'
@@ -26,7 +24,7 @@ import Footer from '@/components/Footer.vue'
 export default {
   name: 'Home',
   components: {
-    HeaderNav, Login, NewMessage, Profil, Wall, WallEditPost, WallModeratePost, Footer
+    HeaderNav, NewMessage, Profil, Wall, WallEditPost, WallModeratePost, Footer
   },
   data(){
     return{
