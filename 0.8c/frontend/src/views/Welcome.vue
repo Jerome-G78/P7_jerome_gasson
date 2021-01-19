5653a838c6c4765117451e2b96af0ae8e7c6a094
Jerome-G78 <67152285+Jerome-G78@users.noreply.github.com>
2021-01-19 16:47:48 +0100

    Updating Frontend - SignIn / LogIn / Profil / NewMessage / Likes / Wall & Comments

---
+++ 0.8c/frontend/src/views/Welcome.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Welcome">
+    <HeaderNav/>
+    <SignIn/>
+    <Login/>
+    <Wall/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderNav from '@/components/HeaderNav.vue'
+import SignIn from '@/components/SignIn.vue'
+import Login from '@/components/Login.vue'
+import Wall from '@/components/Wall.vue'
+import Footer from '@/components/Footer.vue'
+
+
+export default {
+  name: 'Welcome',
+  components: {
+    HeaderNav, SignIn, Login, Wall, Footer
+  },
+  data(){
+    return{
+    // Récupération des variables globales dans vue X
+    Connected: this.$store.state.Connected,
+    }
+  }
+}
+</script>
