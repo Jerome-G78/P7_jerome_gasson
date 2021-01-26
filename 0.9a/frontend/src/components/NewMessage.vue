252218fb76115999145fe062f439230de97cfb71
Jerome-G78 <67152285+Jerome-G78@users.noreply.github.com>
2021-01-26 13:41:38 +0100

    Updating Frontend - New Message (Upload Picture)

--- 0.9a/frontend/src/components/NewMessage.vue
+++ 0.9a/frontend/src/components/NewMessage.vue
@@ -12,12 +12,12 @@
 
                     <div class="form-group">
                         <label for="title">Titre (Minimum 3 Caractères) <span class ="text-danger"> * </span>:</label>
-                        <input @keyup="MsgVerify" type="text" class="form-control" id="title" placeholder="Ajoutez un Titre" name="title" v-model="Ntitle">
+                        <input @keyup="MsgVerify" type="text" class="form-control" id="Title" placeholder="Ajoutez un Titre" name="title" v-model="Data.Ntitle">
                     </div>
 
                     <div class="form-group">
                         <label for="Content"> Contenue du message (Minimum 5 Caractères) <span class ="text-danger"> * </span> :</label>
-                        <textarea @keyup="MsgVerify" class="form-control" id="Content" placeholder="Contenue de votre message" rows="3" v-model="Ncontent"></textarea>
+                        <textarea @keyup="MsgVerify" class="form-control" id="Content" placeholder="Contenue de votre message" rows="3" v-model="Data.Ncontent"></textarea>
                     </div>
 
                     <p class ="text-danger"><small><i>* : Champs obligatoires</i></small></p>
@@ -85,6 +85,9 @@ export default {
                 Connected: this.$store.state.Connected,
                 Loading: this.$store.state.Loading,
                 WallReload: this.$store.state.WallReload,
+
+                Ntitle:this.$store.state.Ntitle,
+                Ncontent:this.$store.state.Ncontent,
                 Npicture:this.$store.state.Npicture,
                 Nattachment: this.$store.state.Nattachment,
 
@@ -101,20 +104,18 @@ export default {
     // Création de la logique du module
     methods:{
         MsgVerify(){
-            let CHKtitle = document.getElementById("title").value;
+            let CHKtitle = document.getElementById("Title").value;
             let CHKContent = document.getElementById("Content").value;
             console.log(CHKtitle, CHKContent);
 
             if(CHKtitle !=''){
                 this.$store.commit('setNtitle', CHKtitle);
-                console.log(this.Ntitle);
             } else {
                 this.$store.commit('setNtitle', '');
             }
 
             if(CHKContent !=''){
                 this.$store.commit('setNcontent', CHKContent);
-                console.log(this.Ncontent);
             } else {
                 this.$store.commit('setNcontent', '');
             }
@@ -128,35 +129,35 @@ export default {
         },
 
         JoinPict(){
-            //WIP
             if(this.uploadFile){
                 this.uploadFile = false;
-                // this.Nattachment = 0;
+                this.Nattachment = 0;
                 this.$store.commit('setNattachment', 0);
                 this.$store.commit('setNpicture','');
                 console.log(this.Nattachment);
 
             } else {
                 this.uploadFile = true;
-                // this.Nattachment = 1;
+                this.Nattachment = 1;
                 this.$store.commit('setNattachment', 1);
                 console.log(this.Data.Nattachment);
             }
         },
 
         Post(){
-            console.log(this.Ntitle, this.Ncontent, this.uploadFile, this.Nattachment);
+            console.log(this.Data.Ntitle, this.Data.Ncontent, this.uploadFile, this.Data.Nattachment);
             this.$store.commit('setLoading', true);
             console.log(this.Data.Loading);
 
-            if(this.Nattachment == 1){
+            if(this.Data.Nattachment == 1){
+                console.log('Attatched');
                 // Récupération du fichier Image
                 let formData = new FormData();
                 let imageFile = document.querySelector("#uploadFile");
                 // console.log(imageFile.value, imageFile.files[0]);
                 formData.append("image",imageFile.files[0]);
                 // Ajout des autres éléments au FormData ( title, content, attachment )
-                formData.append("title",document.getElementById("title").value);
+                formData.append("title",document.getElementById("Title").value);
                 formData.append("content",document.getElementById("Content").value);
                 formData.append("attachment",1);
                 // console.log(FormData);
@@ -185,19 +186,19 @@ export default {
 
                     // Completed
                     document.getElementById("Join").checked = false;
-                    document.getElementById("title").value = '';
+                    document.getElementById("Title").value = '';
                     document.getElementById("Content").value ='';
                     this.subCompleted = true;
-                    this.Nattachment = 0,
+                    this.Nattachment = 0;
                     this.chkCompleted = false;
                     this.subFailure = false;
-                    this.uploadFile = false
+                    this.uploadFile = false;
                     this.subOkay = false;
                     this.subCompleted = false;
+                    this.ResetStats();
 
                     this.$store.commit('setLoading', false);
                     console.log(this.Data.Loading);
-                    this.ResetStats();
 
                     $('#NewMessage').modal('hide');
                     this.$store.commit('setWallReload', true);
@@ -213,6 +214,7 @@ export default {
                 });
 
             } else {
+                console.log('NoAttatched');
                 // Configuration de l'en-tete AXIOS (intégration du token)
                 axios.interceptors.request.use(
                     config => {
@@ -226,7 +228,7 @@ export default {
 
                 // Initialisation de la promesse vers l'API via AXIOS
                 axios.post(this.urlAPI+'/api/messages/new/', {
-                    title: document.getElementById("title").value,
+                    title: document.getElementById("Title").value,
                     content: document.getElementById("Content").value
                 })
                 .then(res =>{
@@ -236,7 +238,7 @@ export default {
 
                     // Completed
                     document.getElementById("Join").checked = false;
-                    document.getElementById("title").value = '';
+                    document.getElementById("Title").value = '';
                     document.getElementById("Content").value ='';
                     this.subCompleted = true;
 
@@ -263,10 +265,11 @@ export default {
         },
 
         ResetStats(){
-            document.getElementById('title').value = '';
+            console.log('Reset...');
             this.$store.commit('setNtitle', '');
-            document.getElementById('Content').value = '';
+            console.log(this.$store.state.Ntitle);
             this.$store.commit('setNcontent', '');
+            console.log(this.$store.state.Ncontent);
             document.getElementById("Join").checked = false;
             this.Nattachment = 0,
             this.chkCompleted = false;
@@ -277,5 +280,19 @@ export default {
             this.$store.commit('setLoading',false);
         }
     },
+    mounted(){
+        document.getElementById('Title').value = '';
+        this.$store.commit('setNtitle', '');
+        document.getElementById('Content').value = '';
+        this.$store.commit('setNcontent', '');
+        document.getElementById("Join").checked = false;
+        this.Nattachment = 0,
+        this.chkCompleted = false;
+        this.subFailure = false;
+        this.uploadFile = false
+        this.subOkay = false;
+        this.subCompleted = false;
+        this.$store.commit('setLoading',false);
+    }
 }
 </script>
