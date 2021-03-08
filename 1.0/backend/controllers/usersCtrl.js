f0c239fbf3ea9e744891a21e24f9823b3eb3ddec
Jerome-G78 <67152285+Jerome-G78@users.noreply.github.com>
2021-03-08 16:48:04 +0100

    Release

--- 1.0/backend/controllers/usersCtrl.js
+++ 1.0/backend/controllers/usersCtrl.js
@@ -14,8 +14,8 @@ const { isDate } = require('util');
 
 // Regex
 const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/ ///^(?=.*\d).{6,}$/;
-// Minimum six characters, at least one uppercase letter, one lowercase letter and one number.
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+// Minimum height characters, at least one uppercase letter, one lowercase letter and one number.
 
 // Routes
 
